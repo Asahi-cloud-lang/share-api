e5a2873c0995a397a2a017cbfb753ad10808c5d5
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2021-03-03 22:21:57 +0900

    second

--- routes/api.php
+++ routes/api.php
@@ -7,26 +7,14 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\LikesController;
-use App\Http\Controllers\CommemntsController;
+use App\Http\Controllers\CommentsController;
 
 Route::apiResource('/shares', SharesController::class);
-
 Route::post('/register', [RegisterController::class, 'post']);
-
 Route::post('/login', [LoginController::class, 'post']);
-
 Route::post('/logout', [LogoutController::class, 'post']);
-
 Route::get('/user', [UsersController::class, 'get']);
-
 Route::put('/user', [UsersController::class, 'put']);
-
 Route::post('/like', [LikesController::class, 'post']);
-
 Route::delete('/like', [LikesController::class, 'delete']);
-
-Route::post('/comment', [CommemntsController::class, 'post']);
-
-
-
-
+Route::post('/comment', [CommentsController::class, 'post']);
